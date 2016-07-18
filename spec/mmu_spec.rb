b4203842279fdd391bb7b38264ef51d4c5956c73
Colby <colby@taplaboratories.com.au>
2016-07-19 00:08:12 +1000

    added specs for timer and mmu

--- spec/mmu_spec.rb
+++ spec/mmu_spec.rb
@@ -3,11 +3,62 @@ require 'spec_helper'
 describe Waterfoul::MMU do
   subject { Waterfoul::MMU.new }
 
+  describe '#write_byte' do
+    context 'writing to 0xFFFFFF' do
+      it 'raises an exception' do
+        expect { subject.write_byte(0xFFFFFF, 0x1) }.to raise_error Waterfoul::MemoryOutOfBounds
+      end
+    end
+
+    context 'when writing to -0x1' do
+      it 'raises an exception' do
+        expect { subject.write_byte(-0x1, 0x1) }.to raise_error Waterfoul::MemoryOutOfBounds
+      end
+    end
+  end
+
+  describe '#read_byte' do
+    context 'reading from 0xFFFFFF' do
+      it 'raises an exception' do
+        expect { subject.read_byte(0xFFFFFF) }.to raise_error Waterfoul::MemoryOutOfBounds
+      end
+    end
+
+    context 'reading from -0x1' do
+      it 'raises an exception' do
+        expect { subject.read_byte(-0x1) }.to raise_error Waterfoul::MemoryOutOfBounds
+      end
+    end
+  end
+
   describe 'DIV register' do
     before { subject.write_byte 0xFF04, 0x25, hardware_operation: true }
     it 'resets when written to' do
       subject.write_byte 0xFF04, 0x35
       expect(subject.read_byte(0xFF04)).to eq 0
     end
+  end
+
+  describe '#read_word' do
+    before do
+      subject.write_byte 0xFFFF, 0x1
+      subject.write_byte 0xFFFE, 0x1
+    end
+
+    it 'reads two bytes from memory into a word' do
+      expect(subject.read_word(0xFFFE)).to eq 0x2
+    end
+  end
+
+  describe '#write_word' do
+    it 'writes 2 bytes into 2 1 byte parts' do
+      subject.write_word 0xFF40, 0xFFAA
+      expect(subject[0xFF40]).to eq 0xAA
+      expect(subject[0xFF41]).to eq 0xFF
+    end
+  end
+
+  context 'DMA transfer' do
+
   end
 end
--- spec/timer_spec.rb
+++ spec/timer_spec.rb
@@ -4,6 +4,45 @@ describe Waterfoul::Timer do
   subject { Waterfoul::Timer.new }
   before { $mmu = Waterfoul::MMU.new }
 
+  describe 'tima register' do
+    context 'when tima is disabled' do
+      before { $mmu.write_byte 0xFF07, 0b011 }
+      it 'does not increment the counter register' do
+        subject.tick(1024)
+        expect($mmu[0xFF05]).to eq 0
+      end
+    end
+
+    context 'when tima is enabled' do
+      before { $mmu.write_byte 0xFF07, 0b111 }
+      it 'increments the counter register' do
+        subject.tick(256)
+        expect($mmu[0xFF05]).to be > 0
+      end
+    end
+
+    context 'with modulo set' do
+      before { $mmu.write_byte 0xFF07, 0b111 }
+      before { $mmu.write_byte 0xFF06, 0x25 }
+      before { $mmu.write_byte 0xFF05, 0xFF }
+      it 'resets counter regsiter to modulo value' do
+        subject.tick(256)
+        expect($mmu[0xFF05]).to eq 0x25
+      end
+    end
+  end
+
+  xdescribe 'timer interrupt' do
+    context 'when counter timer overflows' do
+      before { $mmu.write_byte 0xFF07, 0b111 }
+      before { $mmu.write_byte 0xFF05, 0xFF }
+      it 'triggers timer interrupt' do
+        expect(Interrupt).to receive(:request_interrupt)
+        subject.tick(256)
+      end
+    end
+  end
+
   describe 'divider register' do
     it 'increments the divider register every 256 cycles' do
       subject.tick(256)
@@ -17,6 +56,5 @@ describe Waterfoul::Timer do
         expect($mmu[0xFF04]).to eq 0
       end
     end
-
   end
 end
