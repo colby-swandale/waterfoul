7329ffa754d455470a7da79a64eeef0dffaffe91
Colby <colby@taplaboratories.com.au>
2016-07-18 21:08:20 +1000

    add DIV reset spec and DIV overflow reset spec

--- spec/mmu_spec.rb
+++ spec/mmu_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe Waterfoul::MMU do
-  subject { Waterfoul::MMu.new }
+  subject { Waterfoul::MMU.new }
 
-  describe '#read_byte'
-  describe '#write_byte'
-
-  describe '#read_word'
-  describe '#write_word'
+  describe 'DIV register' do
+    before { subject.write_byte 0xFF04, 0x25, hardware_operation: true }
+    it 'resets when written to' do
+      subject.write_byte 0xFF04, 0x35
+      expect(subject.read_byte(0xFF04)).to eq 0
+    end
+  end
 end
--- spec/timer_spec.rb
+++ spec/timer_spec.rb
@@ -9,5 +9,14 @@ describe Waterfoul::Timer do
       subject.tick(256)
       expect($mmu[0xFF04]).to eq 1
     end
+
+    context 'when DIV register is 255' do
+      before { $mmu.write_byte 0xFF04, 0xFF, hardware_operation: true }
+      it 'resets register to 0 on next increment' do
+        subject.tick(256)
+        expect($mmu[0xFF04]).to eq 0
+      end
+    end
+
   end
 end
