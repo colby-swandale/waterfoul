2bbfd679e267060881ef96b89ef49593a4eec628
Colby <colby@taplaboratories.com.au>
2016-07-21 18:08:54 +1000

    rewote interrupt spec

--- spec/interrupt_spec.rb
+++ spec/interrupt_spec.rb
@@ -2,36 +2,51 @@ require 'spec_helper'
 
 describe Waterfoul::Interrupt do
   before :each do
-    $mmu = double :mmu
+    $mmu = Waterfoul::MMU.new
   end
 
   subject { described_class }
 
   describe '.request_interupt' do
-    before { allow($mmu).to receive(:read_byte).and_return(0x0) }
     context 'with INTERRUPT_VBLANK interrupt' do
-      it 'writes interupt value into IF register' do
-        expect($mmu).to receive(:write_byte).with(0xFF0F, 0x10)
-        subject.request_interupt Waterfoul::Interrupt::INTERRUPT_JOYPAD
+      it 'sets bit for VBLANK interupt in IF register' do
+        subject.request_interrupt Waterfoul::Interrupt::INTERRUPT_JOYPAD
+        expect($mmu[0xFF0F]).to eq 0x10
+      end
+    end
+
+    context 'with INTERRUPT_TIMER set' do
+      it 'sets bit for timer interrupt in IF register' do
+        subject.request_interrupt Waterfoul::Interrupt::INTERRUPT_TIMER
+        expect($mmu[0xFF0F]).to eq 0x4
       end
     end
   end
 
-  describe '.serve_interrupt' do
-    before { allow($mmu).to receive(:read_byte).with(0xFFFF).and_return(0xFF) } # enable all interrupts
-    context 'with all interrupts requested' do
-      before { allow($mmu).to receive(:read_byte).with(0xFF0F).and_return(0xFF) }
-      it 'resets the vblank interrupt' do
-        expect($mmu).to receive(:write_byte).with(0xFF0F, 0xFE)
-        subject.serve_interrupt
+  describe '.pending_interrupt' do
+    context 'when bit 2 of IE and IF register is set' do
+      before { $mmu[0xFF0F] = 0x4 }
+      before { $mmu[0xFFFF] = 0x4 }
+      it 'marks timer interrupt as pending' do
+        expect(subject.pending_interrupt).to eq Waterfoul::Interrupt::INTERRUPT_TIMER
       end
     end
 
-    context' with only joypad interrupt enabled' do
-      before { allow($mmu).to receive(:read_byte).with(0xFF0F).and_return(0x10) }
-      it 'resets the joypad interrupt' do
-        expect($mmu).to receive(:write_byte).with(0xFF0F, 0x0)
-        subject.serve_interrupt
+    context 'when bit 2 of IE register is 0' do
+      before { $mmu[0xFF0F] = 0x4 }
+      before { $mmu[0xFFFF] = 0x0 }
+
+      it 'responds with no interrupts pending' do
+        expect(subject.pending_interrupt).to eq Waterfoul::Interrupt::INTERRUPT_NONE
+      end
+    end
+
+    context 'when 2 interrupts are pending' do
+      before { $mmu[0xFF0F] = 0x3 }
+      before { $mmu[0xFFFF] = 0xFF }
+
+      it 'responds with higher priority interrupt' do
+        expect(subject.pending_interrupt).to eq Waterfoul::Interrupt::INTERRUPT_VBLANK
       end
     end
   end
