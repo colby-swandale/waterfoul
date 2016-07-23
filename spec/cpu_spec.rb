c0780010a57b72da9e47b4b0a2cb2b0e404770e7
Colby <colby@taplaboratories.com.au>
2016-07-23 20:58:36 +1000

    added serve interrupt spec

--- spec/cpu_spec.rb
+++ spec/cpu_spec.rb
@@ -4,6 +4,26 @@ describe Waterfoul::CPU do
   before { $mmu = Waterfoul::MMU.new }
   subject { Waterfoul::CPU.new }
 
+  describe '#serve_interrupt' do
+    before { $mmu[0x0] = 0x0 } # NOP Instruction
+    before { $mmu[0xFFFF] = 0xFF } # Enable All Interrupts
+
+    context 'when timer interrupt' do
+      before { $Mmu[0xFF06] = 0x4 }
+    end
+
+    context 'when no interrupt to serve' do
+      it 'does not move the program counter' do
+        subject.step
+        expect(subject.pc).to eq 0x1
+      end
+
+      it 'does not push onto the stack' do
+        expect { subject.step }.to_not change { subject.sp }
+      end
+    end
+  end
+
   describe '#step' do
     context 'when instruction is NOP' do
       before { expect($mmu).to receive(:read_byte).and_return 0x00 }
