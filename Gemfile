5d20ab60e6e461dea929d63fa162b3ff71b41563
Benoit Daloze <eregontp@gmail.com>
2017-03-05 18:45:47 +0100

    Add --stackprof to profile with stackprof

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in waterfoul.gemspec
 gemspec
+
+gem 'stackprof'
--- lib/waterfoul/cli.rb
+++ lib/waterfoul/cli.rb
@@ -1,11 +1,25 @@
 require "thor"
+
 module Waterfoul
   class CLI < Thor
     desc 'start ROM', 'start the emulator'
     option :skip_boot
+    option :stackprof, :type => :boolean
     def start(rom_file)
       emu = Waterfoul::Emulator.new rom_file, options
-      emu.run
+
+      if options.has_key?('stackprof')
+        require 'stackprof'
+        StackProf.start(mode: :cpu)
+        begin
+          emu.run
+        ensure
+          StackProf.stop
+          StackProf.results('stackprof.dump')
+        end
+      else
+        emu.run
+      end
     end
   end
 end
